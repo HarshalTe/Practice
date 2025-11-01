2c6375e2539f81be0f44f7bacc5a60d84319299c
HarshalTe <harshalterekar509@gmail.com>
2025-11-01 05:35:54 -0700

    practice code

---
+++ src/Hmain.js
@@ -0,0 +1,19 @@
+import CompanyWorkDropdown from "./CompanyWorkDropdown";
+import Da from "./Da";
+import Dropdown1 from "./Dropdown1";
+import DynamicForm from "./DynamicForm";
+import H1 from "./H1";
+import H2 from './H2'
+const Hmain = ()=>{
+     return(
+        <>
+          {/* <H1/> */}
+          {/* <H2/> */}
+          {/* <CompanyWorkDropdown/> */}
+          {/* <Dropdown1/> */}
+          {/* <DynamicForm/> */}
+          <Da/>
+        </>
+     );
+}
+export default Hmain;
