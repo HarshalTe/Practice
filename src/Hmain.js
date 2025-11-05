dee5dc2bb33f0635eeb420e0a1d26834f0230a48
HarshalTe <harshalterekar509@gmail.com>
2025-11-04 23:23:45 -0800

    222ttt

--- src/Hmain.js
+++ src/Hmain.js
@@ -20,6 +20,7 @@ const Hmain = ()=>{
 
           <Da val={20} na={"harshal"} nw={"somthing...."} fullname={"hbt"} ne={"ne"}/>
           <h1>HARSHAL</h1>
+            <h1>HARSHAL2</h1>
 
         </>
      );
