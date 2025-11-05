aa5ae04a48b0a42e9ff2279331514490ab95af18
HarshalTe <harshalterekar509@gmail.com>
2025-11-04 22:31:51 -0800

    ne

--- src/Hmain.js
+++ src/Hmain.js
@@ -12,7 +12,7 @@ const Hmain = ()=>{
           {/* <CompanyWorkDropdown/> */}
           {/* <Dropdown1/> */}
           {/* <DynamicForm/> */}
-          <Da val={20} na={"harshal"} nw={"somthing...."} fullname={"hbt"}/>
+          <Da val={20} na={"harshal"} nw={"somthing...."} fullname={"hbt"} ne={"ne"}/>
         </>
      );
 }
